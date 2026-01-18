fb2438cc6cf85bfbc2db07b68c4e6f4964b2b0b4
Schlaumeier5 <morgenstern.lukas@outlook.com>
2026-01-18 14:59:41 +0100

    Initial commit

---
+++ src/client/java/de/schlaumeier/EMCChatToolsClient.java
@@ -0,0 +1,10 @@
+package de.schlaumeier;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EMCChatToolsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
