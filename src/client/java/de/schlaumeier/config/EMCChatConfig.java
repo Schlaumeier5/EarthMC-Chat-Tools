09eec104b0a7b4975d3d9b6e299cc76eb0b08875
Schlaumeier5 <morgenstern.lukas@outlook.com>
2026-01-27 15:18:04 +0100

    Added known scammers

--- src/client/java/de/schlaumeier/config/EMCChatConfig.java
+++ src/client/java/de/schlaumeier/config/EMCChatConfig.java
@@ -28,7 +28,12 @@ public class EMCChatConfig implements ConfigData {
 
     public float safetyThreshold = 0.70f;
 
-    public List<String> scammerNames = new ArrayList<>();
+    public List<String> scammerNames = new ArrayList<>(List.of(
+        "Panth_r", "Near_r", "LakelessLands", "Dishonesty", "Apportionment", "santosmeow", "Allags", "Skaless", "MalMeteor", "N0vat00n", "Taha_AbuObaida", "fruitapples", "Latvijaa", "Cavify", "josamshaa", "Daniel12615", "Zimin___________",
+        "ItzGnnn", "hielluosymp", "Hashbrown_922", "GabrielCamelot", "Turingly", "Esvicks", "Jteller", "0P2W", "3P2W", "MustangCricket", "aerdisx", "_Duckybara_", "MarkCarney", "chillager_", "RealYoPick1", "VictorrOsimhen",
+        "CornBall_03", "hamza921", "Nf_miracle", "EfeSultan", "TheRealPotatoGuy", "BsidesThat", "TheRealTomatoGuy", "JJMonster5259", "Bigfwuent", "promaniscool", "PolarEMC", "Honduras", "kxretta", "4O4N0TFOUND", "KyOt0",
+        "Continentalia", "needlessrelic", "7Goy", "papayov", "ThundersGoodGirl", "Radical16625", "loukasFYD"
+    ));
 
     public boolean displayAlerts = false;
     public boolean displayScammerAlerts = true;
